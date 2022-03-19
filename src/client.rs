22a05c9bdf8a40698c282a5369b86a4415962639
jeff2857 <jeffliu2857@gmail.com>
2022-03-19 23:53:25 +0800

    fix: refactor functions which get apikey from env

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,12 @@
 pub mod client {
 
+    use log::info;
+
     use hyper::{Client as HttpClient, client::HttpConnector, Method, Request, Response, Body};
 
     use std::env;
 
+    #[derive(Debug)]
     pub struct Client {
         api_key: String,
         secret_key: String,
@@ -15,26 +18,61 @@ pub mod client {
 
     impl Client {
         pub fn new() -> Result<Self, String> {
-            // try to get ApiKey from env
-            let apikey: String;
+            env_logger::init();
+
+            let mut client = Client {
+                api_key: "".to_string(),
+                secret_key: "".to_string(),
+                base_url: &"https://api.binance.com",
+                http_client: HttpClient::new(),
+            };
+
+            match client.get_api_key() {
+                Ok(_) => {return Ok(client);},
+                Err(e) => Err(e),
+            }
+        }
+
+        fn get_api_key(&mut self) -> Result<(), String> {
+            let api_key = self.get_api_key_from_env();
+            if let Ok(k) = api_key {
+                self.api_key = k;
+            } else {
+                return Err("APIKEY not found".to_string());
+            }
+
+            let secret_key = self.get_secret_key_from_env();
+            if let Ok(k) = secret_key {
+                self.secret_key = k;
+            } else {
+                return Err("SECRETKEY not found".to_string());
+            }
+
+            Ok(())
+        }
+
+        fn get_api_key_from_env(&self) -> Result<String, String> {
+            info!("Trying to get APIKEY from env");
+
             let env_apikey = env::var("APIKEY");
 
             if let Ok(k) = env_apikey {
-                apikey = k;
+                Ok(k)
             } else {
-                return Err("ERROR: APIKEY not found".to_string());
+                Err("APIKEY not found".to_string())
             }
+        }
 
-            let http_client = HttpClient::new();
+        fn get_secret_key_from_env(&self) -> Result<String, String> {
+            info!("Trying to get SECRETKEY from env");
 
-            Ok(
-                Client{
-                    api_key: apikey,
-                    secret_key: "".into(),
-                    base_url: "https://api.binance.com".into(),
-                    http_client,
-                }
-            )
+            let env_secretkey = env::var("SECRETKEY");
+
+            if let Ok(k) = env_secretkey {
+                Ok(k)
+            } else {
+                Err("SECRETKEY not found".to_string())
+            }
         }
 
         pub fn set_api_key(&mut self, api_key: String, secret_key: String) {
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
-use colored::*;
+use std::env;
+
+use log::{error, info};
 use binance_sdk_rs::client::client::Client;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    env::set_var("RUST_LOG", "info");
+
+    // test APIKEY and SECRETKEY
+    env::set_var("APIKEY", "MY_APIKEY");
+    env::set_var("SECRETKEY", "MY_SECRETKEY");
+
     let client = Client::new();
 
     if let Err(e) = &client {
-        println!("{}{}", "[E]".red(), e);
+        error!("{}", e);
         return Ok(());
     }
 
     let client = client.unwrap();
+
+    info!("{:?}", client);
     
     let resp = client.get("/api/v3/ping").await?;
 
