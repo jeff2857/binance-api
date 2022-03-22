d8cda9dc3881d42cad609c4a365de65f6d9e9674
jeff2857 <jeffliu2857@gmail.com>
2022-03-22 23:04:04 +0800

    feat: add market api: trades, historicalTrades, aggTrades

--- src/market.rs
+++ src/market.rs
@@ -93,4 +93,82 @@ pub mod market {
         };
         Ok(body_bytes)
     }
+
+    pub async fn trades(client: &Client, symbol: &String, limit: u32) -> Result<Bytes, String> {
+        if limit > 1000 {
+            return Err("limit must be less than or equal to 1000".to_string());
+        }
+
+        let uri = &"/api/v3/trades";
+
+        let param = vec![
+            RequestParam{key: String::from("symbol"), value: String::from(symbol)},
+            RequestParam{key: String::from("limit"), value: limit.to_string()},
+        ];
+        let resp = client.get_with_param(uri, &param).await?;
+        let body_bytes = match hyper::body::to_bytes(resp.into_body()).await {
+            Ok(bytes) => bytes,
+            Err(err) => {
+                return Err(err.to_string());
+            },
+        };
+        Ok(body_bytes)
+    }
+
+    pub async fn historical_trades(client: &Client, symbol: &String, limit: u32, from_id: Option<u64>) -> Result<Bytes, String> {
+        if limit > 1000 {
+            return Err("limit must be less than or equal to 1000".to_string());
+        }
+
+        let uri = &"/api/v3/historicalTrades";
+
+        let mut param = vec![
+            RequestParam{key: String::from("symbol"), value: String::from(symbol)},
+            RequestParam{key: String::from("limit"), value: limit.to_string()},
+        ];
+        if let Some(from_id) = from_id {
+            param.push(RequestParam{key: String::from("fromId"), value: from_id.to_string()});
+        }
+
+        let resp = client.get_with_param(uri, &param).await?;
+        let body_bytes = match hyper::body::to_bytes(resp.into_body()).await {
+            Ok(bytes) => bytes,
+            Err(err) => {
+                return Err(err.to_string());
+            },
+        };
+        Ok(body_bytes)
+    }
+
+    pub async fn agg_trades(client: &Client, symbol: &String, from_id: Option<u64>, start_time: Option<u64>, end_time: Option<u64>, limit: Option<u32>) -> Result<Bytes, String> {
+        let uri = &"/api/v3/aggTrades";
+
+        let mut param = vec![
+            RequestParam{key: String::from("symbol"), value: String::from(symbol)},
+        ];
+        if let Some(from_id) = from_id {
+            param.push(RequestParam{key: String::from("fromId"), value: from_id.to_string()});
+        }
+        if let Some(start_time) = start_time {
+            param.push(RequestParam{key: String::from("startTime"), value: start_time.to_string()});
+        }
+        if let Some(end_time) = end_time {
+            param.push(RequestParam{key: String::from("endTime"), value: end_time.to_string()});
+        }
+        if let Some(mut limit) = limit {
+            if limit > 1000 {
+                limit = 1000;
+            }
+            param.push(RequestParam{key: String::from("limit"), value: limit.to_string()});
+        }
+
+        let resp = client.get_with_param(uri, &param).await?;
+        let body_bytes = match hyper::body::to_bytes(resp.into_body()).await {
+            Ok(bytes) => bytes,
+            Err(err) => {
+                return Err(err.to_string());
+            },
+        };
+        Ok(body_bytes)
+    }
 }
