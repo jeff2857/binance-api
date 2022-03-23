a9b61d207c6bcd0cfeec2db2c4399507bd5555f5
jeff2857 <jeffliu2857@gmail.com>
2022-03-23 22:20:46 +0800

    feat: add wallet api: accountSnapshot

--- src/wallet.rs
+++ src/wallet.rs
@@ -48,7 +48,7 @@ pub mod wallet {
 
     pub async fn capital_all(client: &Client) -> Result<Bytes, String> {
         let uri = &"/sapi/v1/capital/config/getall";
-        // timestamp
+
         let timestamp = get_timestamp();
         let mut param = vec![
             RequestParam{key: String::from("timestamp"), value: timestamp.to_string()},
@@ -57,6 +57,40 @@ pub mod wallet {
         let param_str = param2string(&param);
         let signature = get_signature(&param_str, client.get_secret_key());
         println!("signature: {}", &signature);
+        param.push(RequestParam{key: String::from("signature"), value: signature});
+
+        let resp = client.get_with_param(uri, &param).await?;
+        let body_bytes = match hyper::body::to_bytes(resp.into_body()).await {
+            Ok(bytes) => bytes,
+            Err(err) => {
+                return Err(err.to_string());
+            },
+        };
+
+        Ok(body_bytes)
+    }
+
+    pub async fn account_snapshot(client: &Client, account_type: &String, start_time: Option<u64>, end_time: Option<u64>, limit: Option<u32>) -> Result<Bytes, String> {
+        let uri = &"/sapi/v1/accountSnapshot";
+
+        let mut param = vec![
+            RequestParam{key: String::from("type"), value: String::from(account_type)},
+        ];
+        if let Some(start_time) = start_time {
+            param.push(RequestParam{key: String::from("startTime"), value: start_time.to_string()});
+        }
+        if let Some(end_time) = end_time {
+            param.push(RequestParam{key: String::from("endTime"), value: end_time.to_string()});
+        }
+        if let Some(limit) = limit {
+            param.push(RequestParam{key: String::from("limit"), value: limit.to_string()});
+        }
+
+        let timestamp = get_timestamp();
+        param.push(RequestParam{key: String::from("timestamp"), value: timestamp.to_string()});
+
+        let param_str = param2string(&param);
+        let signature = get_signature(&param_str, client.get_secret_key());
         param.push(RequestParam{key: String::from("signature"), value: signature});
 
         let resp = client.get_with_param(uri, &param).await?;
