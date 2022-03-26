08ceeadc95b86e0bdac0865f07e0773416b1d8b0
jeff2857 <jeffliu2857@gmail.com>
2022-03-26 11:39:48 +0800

    feat: add wallet api: asset/transfer

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,5 +1,5 @@
 pub mod wallet {
-    use std::time::{SystemTime, UNIX_EPOCH};
+    use std::{time::{SystemTime, UNIX_EPOCH}, fmt};
 
     use hmac::{Hmac, Mac};
     use hyper::body::Bytes;
@@ -8,6 +8,38 @@ pub mod wallet {
     use crate::client::client::{Client, RequestParam};
 
     type HmacSha256 = Hmac<Sha256>;
+
+    #[derive(Debug)]
+    #[allow(non_camel_case_types)]
+    pub enum EAssetTransferType {
+        MAIN_UMFUTURE,
+        MAIN_CMFUTURE,
+        MAIN_MARGIN,
+        UMFUTURE_MAIN,
+        UMFUTURE_MARGIN,
+        CMFUTURE_MAIN,
+        MARGIN_MAIN,
+        MARGIN_UMFUTURE,
+        MARGIN_CMFUTURE,
+        CMFUTURE_MARGIN,
+        ISOLATEDMARGIN_MARGIN,
+        MARGIN_ISOLATEDMARGIN,
+        ISOLATEDMARGIN_ISOLATEDMARGIN,
+        MAIN_FUNDING,
+        FUNDING_MAIN,
+        FUNDING_UMFUTURE,
+        UMFUTURE_FUNDING,
+        MARGIN_FUNDING,
+        FUNDING_MARGIN,
+        FUNDING_CMFUTURE,
+        CMFUTURE_FUNDING,
+    }
+
+    impl fmt::Display for EAssetTransferType {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "{:?}", self)
+        }
+    }
 
     fn get_timestamp() -> u64 {
         let now = SystemTime::now();
@@ -532,6 +564,45 @@ pub mod wallet {
         param.push(RequestParam{key: String::from("signature"), value: signature});
     
         let resp = client.get_with_param(uri, &param).await?;
+        let body_bytes = match hyper::body::to_bytes(resp.into_body()).await {
+            Ok(bytes) => bytes,
+            Err(err) => {
+                return Err(err.to_string());
+            },
+        };
+
+        Ok(body_bytes)
+    }
+
+    pub async fn asset_transfer(
+        client: &Client,
+        transfer_type: EAssetTransferType,
+        asset: &str,
+        amount: f64,
+        from_symbol: &Option<&str>,
+        to_symbol: &Option<&str>
+    ) -> Result<Bytes, String> {
+        let uri = &"/sapi/v1/asset/transfer";
+
+        let mut param = vec![
+            RequestParam{key: String::from("type"), value: transfer_type.to_string()},
+            RequestParam{key: String::from("asset"), value: String::from(asset)},
+            RequestParam{key: String::from("amount"), value: amount.to_string()},
+        ];
+
+        if let Some(from_symbol) = from_symbol {
+            param.push(RequestParam{key: String::from("fromSymbol"), value: String::from(*from_symbol)});
+        }
+        if let Some(to_symbol) = to_symbol {
+            param.push(RequestParam{key: String::from("toSymbol"), value: String::from(*to_symbol)});
+        }
+        param.push(RequestParam{key: String::from("timestamp"), value: get_timestamp().to_string()});
+
+        let param_str = param2string(&param);
+        let signature = get_signature(&param_str, client.get_secret_key());
+        param.push(RequestParam{key: String::from("signature"), value: signature});
+    
+        let resp = client.post(uri, &param).await?;
         let body_bytes = match hyper::body::to_bytes(resp.into_body()).await {
             Ok(bytes) => bytes,
             Err(err) => {
